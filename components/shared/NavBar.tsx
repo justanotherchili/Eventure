de872e7ef03b0eddb6abab3387998e4612ea4739
justanotherchili <chiyan.li@hotmail.com>
2024-10-09 07:08:10 +0100

    adds navbar sheet to header with shadcn

--- components/shared/NavBar.tsx
+++ components/shared/NavBar.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 export const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};

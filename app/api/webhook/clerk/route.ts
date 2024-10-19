514b76c36de0263ab4caa46a7c6bcc58665fea3b
justanotherchili <chiyan.li@hotmail.com>
2024-10-19 16:10:22 +0100

    create user in route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -107,7 +107,9 @@
 
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { clerkClient, WebhookEvent } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
+import { createUser } from '@/lib/actions/user.actions'
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -155,11 +157,35 @@ export async function POST(req: Request) {
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
-  const { id } = evt.data
-  const eventType = evt.type
-  if (evt.type === 'user.created') {
-    console.log('userId:', evt.data.id)
+ const { id } = evt.data;
+  const eventType = evt.type;
+ 
+  if(eventType === 'user.created') {
+    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+
+    const user = {
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      username: username!,
+      firstName: first_name,
+      lastName: last_name,
+      photo: image_url,
+    }
+
+    const newUser = await createUser(user);
+
+    if(newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id
+        }
+      })
+    }
+
+    return NextResponse.json({ message: 'OK', user: newUser })
   }
 
+ 
   return new Response('', { status: 200 })
 }
+ 
